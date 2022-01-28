f119b0cf3ccd3c50676c1f2b09796874817e0b53
kiennd <kiend@vnext.com.vn>
2022-01-28 18:03:49 +0700

    bloc observer

--- flutter_bloc_architecture/lib/main.dart
+++ flutter_bloc_architecture/lib/main.dart
@@ -5,7 +5,36 @@ import 'package:flutter_bloc_architecture/presentation/app/index.dart';
 
 void main() async{
   await initInjector();
-  runApp(const App());
+  BlocOverrides.runZoned(
+        () => runApp(const App()),
+    blocObserver: AppBlocObserver(),
+  );
+}
+
+class AppBlocObserver extends BlocObserver {
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    super.onEvent(bloc, event);
+    print(event);
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    print(error);
+    super.onError(bloc, error, stackTrace);
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    print(change);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    print(transition);
+  }
 }
 
 
